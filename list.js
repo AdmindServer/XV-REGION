7a91c563bdbc2719bf040bf12ef90560080d6fd3
Pecinta HPkentang <paskibrajayajaya@gmail.com>
2026-05-12 20:44:32 +0700

    Update list.js

--- list.js
+++ list.js
@@ -1,9 +1,1 @@
-var akunData=[
- {user:'Lonerider99',pass:'starlord99',role:'OWNER',online:false},
- {user:'Bangar',pass:'Bangar09205',role:'OWNER',online:false},
- {user:'Bjorka',pass:'radityafreya2021',role:'OWNER',online:false},
- {user:'zuansuju',pass:'zuan07',role:'OWNER',online:false},
- {user:'Amrol',pass:'AMROLAMROL2006',role:'OWNER',online:false},
- {user:'ZAKIBOYY',pass:'ZAKIBOYY',role:'OWNER',online:false},
- {user:'Yansss',pass:'gagallogin',role:'OWNER',online:false}
-];
+
