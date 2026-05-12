2ef7d5bb3b13c8a55dae6876e89f50ac09a932db
Pecinta HPkentang <paskibrajayajaya@gmail.com>
2026-05-12 23:11:37 +0700

    Create zonk.js

---
+++ zonk.js
@@ -0,0 +1,10 @@
+var akunList=[
+ {user:"bangar",pass:"bangar09205",role:"OWNER",online:false},
+ {user:"bagor99",pass:"bagor",role:"OWNER",online:false},
+ {user:"bjorka",pass:"radityafreya2021",role:"OWNER",online:false},
+ {user:"lonerider99",pass:"starlord99",role:"OWNER",online:false},
+ {user:"zuansuju",pass:"zuan07",role:"OWNER",online:false},
+ {user:"amrol",pass:"amrolamrol2006",role:"OWNER",online:false},
+ {user:"zakiboyy",pass:"zakiboyy",role:"OWNER",online:false},
+ {user:"yansss",pass:"gagallogin",role:"OWNER",online:false}
+];
