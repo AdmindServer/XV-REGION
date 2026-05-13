d56127fa46e1365b6d4e1fb12b92ba0708f6dfd8
Pecinta HPkentang <paskibrajayajaya@gmail.com>
2026-05-13 10:05:19 +0700

    Update zonk.js

--- zonk.js
+++ zonk.js
@@ -1,1 +1,10 @@
-var akunData=[{'user':'99','pass':'99','role':'OWNER','online':![]},{'user':'Bangar','pass':'Bangar09205','role':'OWNER','online':![]},{'user':'Bjorka','pass':'radityafreya2021','role':'OWNER','online':![]},{'user':'zuansuju','pass':'zuan07','role':'OWNER','online':![]},{'user':'Amrol','pass':'AMROLAMROL2006','role':'OWNER','online':![]},{'user':'ZAKIBOYY','pass':'ZAKIBOYY','role':'OWNER','online':![]},{'user':'Yansss','pass':'gagallogin','role':'OWNER','online':![]}];
+var akunList=[
+ {user:"Bangar",pass:"Bangar09205",role:"OWNER",online:false},
+ {user:"Bagor99",pass:"BAGOR",role:"OWNER",online:false},
+ {user:"Bjorka",pass:"radityafreya2021",role:"OWNER",online:false},
+ {user:"Lonerider99",pass:"starlord99",role:"OWNER",online:false},
+ {user:"zuansuju",pass:"zuan07",role:"OWNER",online:false},
+ {user:"Amrol",pass:"AMROLAMROL2006",role:"OWNER",online:false},
+ {user:"ZAKIBOYY",pass:"ZAKIBOYY",role:"OWNER",online:false},
+ {user:"Yansss",pass:"gagallogin",role:"OWNER",online:false}
+];
