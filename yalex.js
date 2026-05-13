53588bcb89de21a684d9656062086b9b60117da8
Pecinta HPkentang <paskibrajayajaya@gmail.com>
2026-05-13 14:11:22 +0700

    Create yalex.js

---
+++ yalex.js
@@ -0,0 +1,10 @@
+var akunList=[
+ {user:"Bangar",pass:"Bangar09205",role:"OWNER",online:false},
+ {user:"Bagor99",pass:"BAGOR",role:"OWNER",online:false},
+ {user:"Bjorka",pass:"radityafreya2021",role:"OWNER",online:false},
+ {user:"Lonerider99",pass:"starlord99",role:"OWNER",online:false},
+ {user:"zuansuju",pass:"zuan07",role:"OWNER",online:false},
+ {user:"Amrol",pass:"AMROLAMROL2006",role:"OWNER",online:false},
+ {user:"ZAKIBOYY",pass:"ZAKIBOYY",role:"OWNER",online:false},
+ {user:"Yansss",pass:"gagallogin",role:"OWNER",online:false}
+];
